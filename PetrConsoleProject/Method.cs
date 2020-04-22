9cc21eaa2de9755d7ed7ff9ae33b0dca076c8b6b
Петр <massyutenko@mail.ru>
2020-04-22 11:55:40 +0300

    testing добавил строчку

--- PetrConsoleProject/Method.cs
+++ PetrConsoleProject/Method.cs
@@ -10,6 +10,7 @@ namespace PetrConsoleProject
     {
         int primer;
         int primer2;
+        int testing;
         public Book[] Massiv_of_books = new Book[2];
 
         public void Create()
